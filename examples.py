b1ebd038b8546794708e7dbad1345195bcde9e7e
James <JamesLatimerTaylor@gmail.com>
2020-12-22 07:56:38 +0000

    add annotated and translated script from https://frank-buss.de/cow.html

--- examples.py
+++ examples.py
@@ -6,5 +6,5 @@ translate.translate("a_equal_b.cow")
 cow.run("a_equal_b.own", "own", 10000)
 cow.run("a_equal_b.cow", "cow", 10000)
 cow.run("frank.cow", "cow", 10000)
-
+translate.translate("frank.cow")
 
